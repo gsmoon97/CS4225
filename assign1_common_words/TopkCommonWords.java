e9c3b8790ac420f7d6f1b61befa5a0c20960f027
Geonsik Moon <gsmoon97@gmail.com>
2022-02-18 22:21:45 +0800

    Test

--- assign1_common_words/TopkCommonWords.java
+++ assign1_common_words/TopkCommonWords.java
@@ -26,11 +26,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class TopkCommonWords {
+	
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
 
 	public static class TokenizerMapper1 extends Mapper<Object, Text, Text, IntWritable> {
-
-		public static final Log log = LogFactory.getLog(TokenizerMapper1.class);
-
+		
 		private Text word = new Text();
 		private IntWritable isFirstFile = new IntWritable(1);
 
@@ -44,8 +56,6 @@ public class TopkCommonWords {
 	}
 
 	public static class TokenizerMapper2 extends Mapper<Object, Text, Text, IntWritable> {
-
-		public static final Log log = LogFactory.getLog(TokenizerMapper2.class);
 
 		private Text word = new Text();
 		private IntWritable isFirstFile = new IntWritable(0);
@@ -65,29 +75,39 @@ public class TopkCommonWords {
 		
 		private IntWritable result = new IntWritable();
 
+//		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+//				throws IOException, InterruptedException {
+//			int firstFreq = 0;
+//			int secondFreq = 0;
+//
+//			for (IntWritable val : values) {
+//				if (val.get() == 1) {
+//					firstFreq++;
+//					System.out.println("key : " + key + " value : " + val.get());
+//					log.info("key : " + key + " value : " + val.get());
+//				} else {
+//					secondFreq++;
+//					System.out.println("key : " + key + " value : " + val.get());
+//					log.info("key : " + key + " value : " + val.get());
+//				}
+//			}
+//			System.out.println("firstFreq : " + firstFreq + " secondFreq : " + secondFreq);
+//			if (firstFreq > 0 && secondFreq > 0) {
+//				result.set(firstFreq < secondFreq ? firstFreq : secondFreq);
+//				System.out.println("writing result for word : " + key);
+//				log.info("writing result for word : " + key);
+//				context.write(key, result);
+//			}
+//		}
+		
 		public void reduce(Text key, Iterable<IntWritable> values, Context context)
 				throws IOException, InterruptedException {
-			int firstFreq = 0;
-			int secondFreq = 0;
-
+			int sum = 0;
 			for (IntWritable val : values) {
-				if (val.get() == 1) {
-					firstFreq++;
-					System.out.println("key : " + key + " value : " + val.get());
-					log.info("key : " + key + " value : " + val.get());
-				} else {
-					secondFreq++;
-					System.out.println("key : " + key + " value : " + val.get());
-					log.info("key : " + key + " value : " + val.get());
-				}
+				sum += val.get();
 			}
-			System.out.println("firstFreq : " + firstFreq + " secondFreq : " + secondFreq);
-			if (firstFreq > 0 && secondFreq > 0) {
-				result.set(firstFreq < secondFreq ? firstFreq : secondFreq);
-				System.out.println("writing result for word : " + key);
-				log.info("writing result for word : " + key);
-				context.write(key, result);
-			}
+			result.set(sum);
+			context.write(key, result);
 		}
 	}
 
@@ -95,13 +115,14 @@ public class TopkCommonWords {
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf, "top common words");
 		job.setJarByClass(TopkCommonWords.class);
-//    job.setMapperClass(TokenizerMapper.class);
+		job.setMapperClass(TokenizerMapper.class);		
 		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TokenizerMapper1.class);
-		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TokenizerMapper2.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+//		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TokenizerMapper1.class);
+//		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TokenizerMapper2.class);
 		// MultipleInputs.addInputPath(job, new Path(args[2]), TextInputFormat.class);
 		FileOutputFormat.setOutputPath(job, new Path(args[3]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
